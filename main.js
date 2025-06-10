b9994e17d864c5c24a63ba7648135edc6adc491c
Shreya <153142441+shreya-0718@users.noreply.github.com>
2025-06-10 08:05:33 +0000

    Recording functionality, added ✨ aesthetics ✨

--- main.js
+++ main.js
@@ -45,6 +45,9 @@ const colorpicker2 = document.getElementById("color2");
 // volume control variables
 const vol_slider = document.getElementById("vol-slider");
 
+// recording variables
+const recording_toggle = document.getElementById('record');
+
 function frequency(pitch) {
     freq = pitch / 10000;
     
@@ -108,7 +111,7 @@ function drawWave(){
 }
 
 function line() {
-    amplitude = vol_slider.value; // adjust amplitude based on volume slider
+    amplitude = vol_slider.value*.6; // adjust amplitude based on volume slider
     y = height/2 + (amplitude * Math.sin(x * 2 * Math.PI * freq * (0.5 * length)));
     
     const gradient = ctx.createLinearGradient(0, 0, width, 0);
@@ -129,3 +132,50 @@ function line() {
     }
 }
 
+// set up audio recording
+var blob, recorder = null;
+var chunks = [];
+
+function startRecording(){
+    const canvasStream = canvas.captureStream(20); // Frame rate of canvas
+    const audioDestination = audioCtx.createMediaStreamDestination();
+    gainNode.connect(audioDestination);
+
+    const combinedStream = new MediaStream();
+    // Add in video data
+
+    canvasStream.getVideoTracks().forEach(track => combinedStream.addTrack(track));
+    audioDestination.stream.getAudioTracks().forEach(track => combinedStream.addTrack(track))
+
+    recorder = new MediaRecorder(combinedStream, { mimeType: 'video/webm' });
+
+    recorder.ondataavailable = e => {
+        if (e.data.size > 0) {
+            chunks.push(e.data);
+        }
+    };
+
+    recorder.onstop = () => {
+        const blob = new Blob(chunks, { type: 'video/webm' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'recording.webm';
+        a.click();
+        URL.revokeObjectURL(url);
+    };
+
+    recorder.start();
+}
+
+var is_recording = false;
+function toggle(){
+    is_recording = !is_recording;
+    if (is_recording) {
+        recording_toggle.innerHTML = " Stop Recording 🛑 ";
+        startRecording();
+    } else {
+        recording_toggle.innerHTML = " Start Recording 🎙️ ";
+        recorder.stop();
+    }
+}
